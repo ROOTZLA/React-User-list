c663f9f2605c4434054934cf2cabb2ff30cf17f6
ROOTZLA <148280991+ROOTZLA@users.noreply.github.com>
2025-07-23 01:34:10 +0300

    Update UserForm.tsx

--- src/UserForm.tsx
+++ src/UserForm.tsx
@@ -120,7 +120,7 @@ export default function UserForm({
             onChange={(event) => setValueEmail(event.target.value)}
           />
           {valueEmail && !valueEmailValid && (
-            <p className="text-red-600 text-[12px]">Please enter a valid email address.</p>
+            <p className="text-red-600 text-[12px]">Please enter a valid email address</p>
           )}
         </div>
       </div>
@@ -136,7 +136,7 @@ export default function UserForm({
             onChange={(event) => setValueWebsite(event.target.value)}
           />
           {valueWebsite && !valueWebsiteValid && (
-            <p className="text-red-600 text-[12px]">Please enter a valid Website URL.</p>
+            <p className="text-red-600 text-[12px]">Please enter a valid Website URL</p>
           )}
         </div>
       </div>
